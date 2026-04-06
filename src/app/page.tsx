926de455ff830302e83deb59c83e693ade49602d
LAii980514 <kim86886245@gmail.com>
2026-04-07 03:17:27 +0900

    chore: swap order of Illustrations and Timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
       <FeaturedProjects />
       <ProjectsGrid />
       <About />
-      <Illustrations />
       <Timeline />
+      <Illustrations />
       <Gallery />
       <Contact />
       <Footer />
