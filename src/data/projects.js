9dc68f81da481b1b8f09a8595017c799fd47c15c
LAii980514 <kim86886245@gmail.com>
2026-06-01 06:43:09 +0900

    Update project 2 to polishing state, add access block for production, add mainImage placeholder

--- src/data/projects.js
+++ src/data/projects.js
@@ -31,7 +31,7 @@ export const mainProjects = [
   },
   {
     id: 2,
-    title: "마법소녀의 기억",
+    title: "마법소녀의 기억 (폴리싱 중)",
     genre: "내러티브 액션 RPG",
     engine: "언리얼 엔진 5.5",
     mapSize: "37일",
@@ -44,6 +44,7 @@ export const mainProjects = [
     tags: ["어반판타지", "일본", "창작 레벨", "마법소녀"],
     alignImage: 'left',
     videoId: 'eioYulMp_5k',
+    mainImage: '/project2_main.jpg',
     docLink: '#', // TODO: Add real Google Drive link
     coreExperience: '탁 트인 환경에서의 탐험과 거대 보스전의 내러티브 경험',
     references: '호라이즌 제로 던, 원신, 니어 오토마타'
